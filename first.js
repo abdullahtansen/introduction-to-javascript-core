9b3b84e925888762c20fc6a6008885d4ac68dcbc
abdullahtansen <abdullahaltansen899@gmail.com>
2022-12-03 22:19:29 +0600

    array concept

---
+++ first.js
@@ -0,0 +1,16 @@
+// first problem solved
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var bangla = 99.50;
+
+var subjectCount = 5;
+var fiveSubjectTotalMarks = (mathematics+biology+chemistry+physics+bangla);
+var avarageMarks = parseFloat((fiveSubjectTotalMarks/subjectCount).toFixed(2));
+console.log(avarageMarks);
+
+// second problem solved
+var sarahMathematicsNumber = 119;
+var reminder = sarahMathematicsNumber % 5;
+console.log(reminder);
---
+++ index.js
@@ -0,0 +1,12 @@
+var numbers = [45,67,20,48.9,100,1999];
+// get element by index
+var element = numbers[1];
+console.log(element);
+
+// set element value by index
+numbers[1] = 77;
+console.log(numbers);
+
+// find index of an element
+var positionIndex = numbers.indexOf(1999);
+console.log(positionIndex);
