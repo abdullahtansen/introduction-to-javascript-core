e1de97d7195c3d64df5219b0858d1945f5087653
abdullahtansen <abdullahaltansen899@gmail.com>
2022-12-05 16:18:33 +0600

    loop-for-loop-while-loop

---
+++ loops-practice-problem/even.js
@@ -0,0 +1,6 @@
+// target: 10 to all event numbers
+var number = 0;
+while (number <= 20){
+    console.log(number)
+    number = number +2;
+}
---
+++ loops-practice-problem/while.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+
+while(roastGiven < 7) {
+    console.log('Roast Den, Please !!!!!!!!!!');
+    // roastGiven += 1;
+    console.log(roastGiven);
+    roastGiven++; 
+}
